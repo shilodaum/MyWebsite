62ee78439c7d4b01719aa60609f360e5be6177f8
shilodaum <shilodaum@gmail.com>
2020-10-23 02:08:12 +0300

    created file

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>My Website</title>
+<script src="./jsfunctions.js"></script>
+<link rel="icon" href="./logo.png">
+<script src="jquery-3.5.1.min.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
